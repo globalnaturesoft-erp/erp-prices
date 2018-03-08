b7437f3498a4f88c9cf461eb538be504fc10108e
Hung Nguyen <hungnt@hoangkhang.com.vn>
2018-03-08 16:43:12 +0700

    update get price by product

---
+++ app/decorators/models/products/product_decorator.rb
@@ -0,0 +1,44 @@
+Erp::Products::Product.class_eval do
+  def get_default_purchase_price(options={})
+    Erp::Prices::Price.get_by_product(
+      product_id: self.id,
+      contact_id: options[:contact_id],
+      category_id: self.category_id,
+      properties_value_id: self.get_properties_value(Erp::Products::Property.getByName(Erp::Products::Property::NAME_DUONG_KINH)),
+      quantity: options[:quantity],
+      type: Erp::Prices::Price::TYPE_PURCHASE
+    )
+  end  
+  
+  def get_default_sales_price(options={})
+    Erp::Prices::Price.get_by_product(
+      product_id: self.id,
+      contact_id: options[:contact_id],
+      category_id: self.category_id,
+      properties_value_id: self.get_properties_value(Erp::Products::Property.getByName(Erp::Products::Property::NAME_DUONG_KINH)),
+      quantity: options[:quantity],
+      type: Erp::Prices::Price::TYPE_SALES
+    )
+  end
+  
+  def get_related_purchase_prices_rows(options={})
+    Erp::Prices::Price.get_related_prices_rows(
+      product_id: self.id,
+      contact_id: options[:contact_id],
+      category_id: self.category_id,
+      properties_value_id: self.get_properties_value(Erp::Products::Property.getByName(Erp::Products::Property::NAME_DUONG_KINH)),
+      type: Erp::Prices::Price::TYPE_PURCHASE
+    )
+  end
+  
+  def get_related_sales_prices_rows(options={})
+    Erp::Prices::Price.get_related_prices_rows(
+      product_id: self.id,
+      contact_id: options[:contact_id],
+      category_id: self.category_id,
+      properties_value_id: self.get_properties_value(Erp::Products::Property.getByName(Erp::Products::Property::NAME_DUONG_KINH)),
+      type: Erp::Prices::Price::TYPE_SALES
+    )
+  end
+end
+
